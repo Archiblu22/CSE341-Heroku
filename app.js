5ab680e7c86b804d32acc13727763e9fd906e6a8
archiblue22 <paguirre@byui.edu>
2021-09-23 13:49:50 -0600

    commit

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-
-const users = [];
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
-    res.render('index', {pageTitle: 'Add User'});
-});
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.get('/users', (req, res, next) => {
-    res.render('users', {
-        pageTitle: 'Users',
-        users: users,
-        hasUsers: users.length > 0
-    });
-});
-
-app.post('/add-user', (req, res, next) => {
-    users.push({name: req.body.username});
-    res.redirect('/users');
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(PORT);
