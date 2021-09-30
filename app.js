4a85c0ce8b80f20729c9fae96f6971f36a23ef0e
archiblue22 <paguirre@byui.edu>
2021-09-29 23:59:20 -0600

    commit

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+
 const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -12,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
