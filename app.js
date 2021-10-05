07a2bda200d7803d833b31ff1c3a78356c49e631
archiblue22 <paguirre@byui.edu>
2021-10-05 13:55:50 -0600

    commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:3000
 const path = require('path');
+const cors = require('cors')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,6 +23,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+const corsOptions = {
+    origin: "https://cse341ecomm.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://paguirre:8199275Aa@cluster0.gfhn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 app.use(errorController.get404);
 
-app.listen(PORT);
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
