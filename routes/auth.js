82f133f1c82f2a20bacfa72790e861655d166953
archiblue22 <paguirre@byui.edu>
2021-10-20 13:01:06 -0600

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { check, body } = require('express-validator');
+const {
+    check,
+    body
+} = require('express-validator');
 
 const authController = require('../controllers/auth');
 const User = require('../models/user');
@@ -11,53 +14,72 @@ router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 
 router.post(
-  '/login',
-  [
-    body('email')
-      .isEmail()
-      .withMessage('Please enter a valid email address.')
-      .normalizeEmail(),
-    body('password', 'Password has to be valid.')
-      .isLength({ min: 5 })
-      .isAlphanumeric()
-      .trim()
-  ],
-  authController.postLogin
+    '/login',
+    [
+        body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email address.')
+        .normalizeEmail(),
+        body('password', 'Password has to be valid.')
+        .isLength({
+            min: 5
+        })
+        .isAlphanumeric()
+        .trim()
+    ],
+    authController.postLogin
 );
 
 router.post(
-  '/signup',
-  [
-    check('email')
-      .isEmail()
-      .withMessage('Please enter a valid email.')
-      .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(userDoc => {
-          if (userDoc) {
-            return Promise.reject(
-              'E-Mail exists already, please pick a different one.'
-            );
-          }
-        });
-      })
-      .normalizeEmail(),
-    body(
-      'password',
-      'Please enter a password with only numbers and text and at least 5 characters.'
-    )
-      .isLength({ min: 5 })
-      .isAlphanumeric()
-      .trim(),
-    body('confirmPassword')
-      .trim()
-      .custom((value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error('Passwords have to match!');
-        }
-        return true;
-      })
-  ],
-  authController.postSignup
+    '/signup',
+    [
+        body(
+            'name',
+            'Please enter your full name.'
+        )
+        .isLength({
+            min: 4
+        })
+        .isAlpha()
+        .trim(),
+        check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {
+            req
+        }) => {
+            return User.findOne({
+                email: value
+            }).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject(
+                        'E-Mail exists already, please pick a different one.'
+                    );
+                }
+            });
+        })
+        .normalizeEmail(),
+        body(
+            'password',
+            'Please enter a password with only numbers and text and at least 5 characters.'
+        )
+        .isLength({
+            min: 5
+        })
+        .isAlphanumeric()
+        .trim(),
+        body('confirmPassword')
+        .trim()
+        .custom((value, {
+            req
+        }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup
 );
 
 router.post('/logout', authController.postLogout);
